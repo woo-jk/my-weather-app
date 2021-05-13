726ef31105a00bf614b0afbb41d8312a3e9c8936
Lainfox <lainfox@gmail.com>
2021-05-13 22:50:00 +0900

    Cities component with async/await

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import CityList from "../components/CityList";
+
 function Cities() {
+  const API_URL = "http://riotkr.mockable.io/weather-crawlers/cities";
+  const [cityState, setCityState] = useState([]);
+
+  // hooks
+  useEffect(() => {
+    console.log("cities component mounted!");
+    getCities();
+  }, []); // once
+
+  async function getCities() {
+    const cities = await fetch(API_URL).then((res) => res.json());
+    console.log(cities);
+
+    setCityState(cities);
+  }
+
   return (
     <div>
       <h1>Cities</h1>
+      <p>{API_URL}</p>
+      <CityList cities={cityState} />
     </div>
   );
 }
