9c96369c3a1ce3c2505a76099b8127f8ac803644
Lainfox <lainfox@gmail.com>
2021-05-13 21:47:31 +0900

    Wrap router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const city = "Seoul";
+
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header region="KR" cityName={city} />
+        <ul className="menu">
+          <li>Home</li>
+          <li>About</li>
+          <li>Cities</li>
+        </ul>
+      </div>
+    </Router>
   );
 }
 
