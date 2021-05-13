e2726aa1eb169938820db038e8e1e29e1dab1a58
Lainfox <lainfox@gmail.com>
2021-05-13 22:18:53 +0900

    Add routed pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Cities from "./pages/Cities";
+import Home from "./pages/Home";
 
 function App() {
   const city = "Seoul";
@@ -9,11 +12,29 @@ function App() {
     <Router>
       <div className="App">
         <Header region="KR" cityName={city} />
-        <ul className="menu">
-          <li>Home</li>
-          <li>About</li>
-          <li>Cities</li>
+        <ul className="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cities">Cities</Link>
+          </li>
         </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/cities">
+            <Cities />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
